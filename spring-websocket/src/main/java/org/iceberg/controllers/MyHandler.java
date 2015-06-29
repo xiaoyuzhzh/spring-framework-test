9f8be802d3a7e58fd2592dba0fa24f473cd39cbd
xiaoyuzhzh <xiaoyuzhzh@163.com>
2015-06-29 15:13:18 +0800

    项目编码修改

--- spring-websocket/src/main/java/org/iceberg/controllers/MyHandler.java
+++ spring-websocket/src/main/java/org/iceberg/controllers/MyHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class MyHandler extends TextWebSocketHandler {
 	 @Override
 	 public void handleTextMessage(WebSocketSession session, TextMessage message) {
-		 System.out.println("进入myhandler了");
+		 System.out.println("进入myhandler了"); 
 		 System.out.println(session.getRemoteAddress()+" info:  "+message.getPayload());
 		 try {
 			session.sendMessage(new TextMessage("这个是返回的信息。"));
